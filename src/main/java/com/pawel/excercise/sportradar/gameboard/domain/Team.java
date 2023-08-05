a99ed101651d483ca9b5ce1d12c0e0130875b076
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 00:29:52 +0200

    Team name field

--- src/main/java/com/pawel/excercise/sportradar/gameboard/domain/Team.java
+++ src/main/java/com/pawel/excercise/sportradar/gameboard/domain/Team.java
@@ -1,4 +1,13 @@
 package com.pawel.excercise.sportradar.gameboard.domain;
 
 public class Team {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
