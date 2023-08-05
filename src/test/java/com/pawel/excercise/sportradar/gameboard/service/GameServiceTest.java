fba4e152294a1d5da411ac842aede0c3ab1c1ee6
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 01:35:58 +0200

    Add test for creating gameService

---
+++ src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
@@ -0,0 +1,11 @@
+package com.pawel.excercise.sportradar.gameboard.service;
+
+import org.junit.jupiter.api.Test;
+
+public class GameServiceTest {
+
+    @Test
+    void shouldBeAbleToCreateNewGameService() {
+        GameService gameService = new GameService();
+    }
+}
