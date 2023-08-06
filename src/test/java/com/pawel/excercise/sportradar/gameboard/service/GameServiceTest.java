da4f973d8ab939bc3f98c8620fd742dfff0701e2
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 03:24:15 +0200

    Test GameService: get current game

--- src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
+++ src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
@@ -5,6 +5,8 @@ import com.pawel.excercise.sportradar.gameboard.domain.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.util.Pair;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameServiceTest {
 
     @Test
@@ -17,5 +19,22 @@ public class GameServiceTest {
 
         GameService gameService = new GameService(game, gameBoard);
     }
+
+    @Test
+    void shouldReturnGameTeams() {
+        Team polishTeam = new Team();
+        Team germanTeam = new Team();
+        Pair<Team, Team> game = Pair.of(polishTeam, germanTeam);
+
+        GameBoard gameBoard = new GameBoard();
+
+        GameService gameService = new GameService(game, gameBoard);
+        Pair<Team, Team> currentGame = gameService.getGame();
+
+        assertEquals(currentGame.getFirst(), polishTeam);
+        assertEquals(currentGame.getSecond(), germanTeam);
+    }
+
+
 
 }
