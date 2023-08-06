9287cc0af35735f265647636c68407c4460d57e4
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 17:21:02 +0200

    Implementaion of adding new game to GameBoard

--- src/main/java/com/pawel/excercise/sportradar/gameboard/domain/GameBoard.java
+++ src/main/java/com/pawel/excercise/sportradar/gameboard/domain/GameBoard.java
@@ -1,11 +1,29 @@
 package com.pawel.excercise.sportradar.gameboard.domain;
 
 import com.pawel.excercise.sportradar.gameboard.utils.GameObserver;
+import org.springframework.data.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameBoard implements GameObserver {
+
+    private List<Pair<Team, Team>> allGames = new ArrayList<>();
+
+    public List<Pair<Team, Team>> getAllGames() {
+        return allGames;
+    }
+
+    public GameBoard() {
+    }
 
     @Override
     public void update() {
         //some implementation
+    }
+
+    @Override
+    public void addGameToBard(Pair<Team, Team> game) {
+        allGames.add(game);
     }
 }
