4f75a705d3529e3481c722149c55ca2c555c7fdd
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 02:24:41 +0200

    Test for creating GameService with arguments

--- src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
+++ src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
@@ -1,6 +1,8 @@
 package com.pawel.excercise.sportradar.gameboard.service;
 
+import com.pawel.excercise.sportradar.gameboard.domain.Team;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.util.Pair;
 
 public class GameServiceTest {
 
@@ -8,4 +10,16 @@ public class GameServiceTest {
     void shouldBeAbleToCreateNewGameService() {
         GameService gameService = new GameService();
     }
+
+    @Test
+    void shouldBeAbleToCreateGameServiceWithDependencies() {
+        Team polishTeam = new Team();
+        Team germanTeam = new Team();
+        Pair<Team, Team> game = Pair.of(polishTeam, germanTeam);
+
+        GameBoard gameBoard = new GameBoard();
+
+        GameService gameService = new GameService(game, gameBoard);
+    }
+
 }
