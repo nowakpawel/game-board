67e476046b9ee2bd64e60350ed67227f3b2b3aac
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 02:43:31 +0200

    GameService: add all args constructor

--- src/main/java/com/pawel/excercise/sportradar/gameboard/service/GameService.java
+++ src/main/java/com/pawel/excercise/sportradar/gameboard/service/GameService.java
@@ -1,6 +1,18 @@
 package com.pawel.excercise.sportradar.gameboard.service;
 
+import com.pawel.excercise.sportradar.gameboard.domain.GameBoard;
+import com.pawel.excercise.sportradar.gameboard.domain.Team;
+import org.springframework.data.util.Pair;
+
 public class GameService {
+    private Pair<Team, Team> game;
+    private GameBoard gameBoard;
+
+    public GameService(Pair<Team, Team> game, GameBoard gameBoard) {
+        this.game = game;
+        this.gameBoard = gameBoard;
+    }
+
     public GameService() {
     }
 }
