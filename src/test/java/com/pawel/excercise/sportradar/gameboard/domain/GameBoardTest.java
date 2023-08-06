85a66cbbd471063ee08fbbbc114aa712c9e768ac
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 16:56:47 +0200

    Test for adding new game to board

--- src/test/java/com/pawel/excercise/sportradar/gameboard/domain/GameBoardTest.java
+++ src/test/java/com/pawel/excercise/sportradar/gameboard/domain/GameBoardTest.java
@@ -2,7 +2,9 @@ package com.pawel.excercise.sportradar.gameboard.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.util.Pair;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GameBoardTest {
@@ -15,6 +17,16 @@ class GameBoardTest {
 
     @Test
     void shouldBeAbleToGetAllGames() {
-        assertNotNull(underTest.getAllGames);
+        assertNotNull(underTest.getAllGames());
+    }
+
+    @Test
+    void shouldBeAbleToAddGameToBoard() {
+        Team polishTeam = new Team();
+        Team germanTeam = new Team();
+        Pair<Team, Team> newGame = Pair.of(polishTeam, germanTeam);
+
+        underTest.addGameToBard(newGame);
+        assertEquals(underTest.getAllGames().size(), 1);
     }
 }
