57a40971e76a33b7dfee993939c5773803dcc03f
nowakpawel <kontakt.nowakpawel@gmail.com>
2023-08-06 04:04:41 +0200

    GameServiceTest refactor

--- src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
+++ src/test/java/com/pawel/excercise/sportradar/gameboard/service/GameServiceTest.java
@@ -2,52 +2,44 @@ package com.pawel.excercise.sportradar.gameboard.service;
 
 import com.pawel.excercise.sportradar.gameboard.domain.GameBoard;
 import com.pawel.excercise.sportradar.gameboard.domain.Team;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.util.Pair;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameServiceTest {
-
-    @Test
-    void shouldBeAbleToCreateGameServiceWithDependencies() {
+    private GameService underTest = null;
+    @BeforeEach
+    void setUp() {
         Team polishTeam = new Team();
         Team germanTeam = new Team();
         Pair<Team, Team> game = Pair.of(polishTeam, germanTeam);
 
         GameBoard gameBoard = new GameBoard();
 
-        GameService gameService = new GameService(game, gameBoard);
+        underTest = new GameService(game, gameBoard);
+    }
+
+    @Test
+    void shouldBeAbleToCreateGameServiceWithDependencies() {
+        assertThat(underTest).isNotNull();
     }
 
     @Test
     void shouldReturnGameTeams() {
-        Team polishTeam = new Team();
-        Team germanTeam = new Team();
-        Pair<Team, Team> game = Pair.of(polishTeam, germanTeam);
+        Pair<Team, Team> currentGame = underTest.getGame();
 
-        GameBoard gameBoard = new GameBoard();
-
-        GameService gameService = new GameService(game, gameBoard);
-        Pair<Team, Team> currentGame = gameService.getGame();
-
-        assertEquals(currentGame.getFirst(), polishTeam);
-        assertEquals(currentGame.getSecond(), germanTeam);
+       assertThat(currentGame.getFirst()).isInstanceOf(Team.class);
+       assertThat(currentGame.getSecond()).isInstanceOf(Team.class);
     }
 
     @Test
     void shouldBeAbleToInitialGame() {
-        Team polishTeam = new Team();
-        Team germanTeam = new Team();
-        Pair<Team, Team> game = Pair.of(polishTeam, germanTeam);
+        underTest.startGame();
 
-        GameBoard gameBoard = new GameBoard();
-
-        GameService gameService = new GameService(game, gameBoard);
-
-        gameService.startGame();
-
-        Pair<Team, Team> currentGame = gameService.getGame();
+        Pair<Team, Team> currentGame = underTest.getGame();
 
         assertEquals(currentGame.getFirst().getScore(), 0);
         assertEquals(currentGame.getSecond().getScore(), 0);
